6c551e04d64b1d5b88c1c58f97e1b8e8175b1196
Henrique Artur <contato@henriqueartur.com>
2023-06-10 21:28:21 -0300

    :sparkles: Add favorites history

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,8 +1,5 @@
-import 'dart:js';
-
 import 'package:first_flutter_app/favorites_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 import 'generator_page.dart';
 
@@ -17,76 +14,105 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     var colorScheme = Theme.of(context).colorScheme;
-    Widget page = pageToRender(selectedIndex);
+    Widget page = selectPage(selectedIndex);
+
     // The container for the current page, with its background color
     // and subtle switching animation.
-    var mainArea = mainAreaBoxComponent(colorScheme, page);
+    var mainArea = ColoredBox(
+      color: colorScheme.surfaceVariant,
+      child: AnimatedSwitcher(
+        duration: Duration(milliseconds: 200),
+        child: page,
+      ),
+    );
 
     return Scaffold(
-      body: LayoutBuilder(builder: (context, constraints) {
-        return constraints.maxWidth < 450
-            ? mainAreaCompactComponent(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 450) {
+            return mobileRender(
                 mainArea,
                 selectedIndex,
-                (value) => setState(() => (selectedIndex = value)),
-              )
-            : mainAreaComponent(mainArea, selectedIndex,
-                (value) => setState(() => (selectedIndex = value)));
-      }),
+                (value) => {
+                      setState(() {
+                        selectedIndex = value;
+                      })
+                    });
+          } else {
+            return desktopRender(
+                mainArea,
+                selectedIndex,
+                (value) => {
+                      setState(() {
+                        selectedIndex = value;
+                      })
+                    },
+                constraints);
+          }
+        },
+      ),
     );
   }
 }
 
-Widget pageToRender(int selectedIndex) {
-  switch (selectedIndex) {
+Widget selectPage(int pageIndex) {
+  switch (pageIndex) {
     case 0:
       return GeneratorPage();
     case 1:
       return FavoritesPage();
     default:
-      throw UnimplementedError('no widget for $selectedIndex');
+      throw UnimplementedError('no widget for $pageIndex');
   }
 }
 
-ColoredBox mainAreaBoxComponent(ColorScheme colorScheme, Widget page) {
-  return ColoredBox(
-    color: colorScheme.surfaceVariant,
-    child: AnimatedSwitcher(
-      duration: Duration(milliseconds: 200),
-      child: page,
-    ),
+Widget mobileRender(
+    ColoredBox mainArea, int selectedIndex, Function onTapCallback) {
+  return Column(
+    children: [
+      Expanded(child: mainArea),
+      SafeArea(
+        child: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favorites',
+            ),
+          ],
+          currentIndex: selectedIndex,
+          onTap: (value) => onTapCallback(value),
+        ),
+      )
+    ],
   );
 }
 
-Widget mainAreaComponent(
-    ColoredBox mainArea, int selectedIndex, Function setIndex) {
-  return Column(children: [
-    Expanded(child: mainArea),
-    SafeArea(
-        child: BottomNavigationBar(
-      items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Favorites")
-      ],
-      currentIndex: selectedIndex,
-      onTap: (value) => setIndex(value),
-    )),
-    Expanded(child: mainArea),
-  ]);
-}
-
-Widget mainAreaCompactComponent(
-    ColoredBox mainArea, int selectedIndex, Function setIndex) {
-  return Column(children: [
-    Expanded(child: mainArea),
-    SafeArea(
-        child: BottomNavigationBar(
-      items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Favorites")
-      ],
-      currentIndex: selectedIndex,
-      onTap: (value) => setIndex(value),
-    )),
-  ]);
+Widget desktopRender(ColoredBox mainArea, int selectedIndex,
+    Function onTapCallback, BoxConstraints constraints) {
+  return Row(
+    children: [
+      SafeArea(
+        child: NavigationRail(
+          extended: constraints.maxWidth >= 600,
+          destinations: [
+            NavigationRailDestination(
+              icon: Icon(Icons.home),
+              label: Text('Home'),
+            ),
+            NavigationRailDestination(
+              icon: Icon(Icons.favorite),
+              label: Text('Favorites'),
+            ),
+          ],
+          selectedIndex: selectedIndex,
+          onDestinationSelected: (value) => onTapCallback(value),
+        ),
+      ),
+      Expanded(child: mainArea),
+    ],
+  );
 }
